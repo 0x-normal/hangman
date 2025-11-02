09599e60a5e3a61be84b926bb435650b76706d7e
0x-normal <49248760+0x-normal@users.noreply.github.com>
2025-11-02 15:21:38 +0100

    Remove redirects and output configuration
    Remove Next.js configuration for redirects and output.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // ... other configs ...
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/hangman.html', // Use the path relative to the public directory
-        permanent: true,
-      },
-    ];
-  },
-  output: 'export',
-};
 
-module.exports = nextConfig;
